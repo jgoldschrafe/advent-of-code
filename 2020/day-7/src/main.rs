dac664929d835ad64144ec803f41f6e4e875d8bd
Jeff Goldschrafe <jeff@holyhandgrenade.org>
2020-12-13 01:29:43 -0500

    day-7: Part 2

--- 2020/day-7/src/main.rs
+++ 2020/day-7/src/main.rs
@@ -1,10 +1,9 @@
-/*
- * INCOMPLETE
- */
-
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::io;
 use std::io::prelude::*;
+
+use petgraph::prelude::*;
 
 #[derive(Debug)]
 struct Adjacency {
@@ -27,7 +26,7 @@ fn parse_line(line: &str) -> Vec<Adjacency> {
         })
         .filter_map(|(qty_str, child_name)| {
             match qty_str {
-                "no" => None,
+                "0" | "no" => None,
                 _ => Some(Adjacency {
                     parent_name: parent_name.clone(),
                     child_name: child_name.to_string(),
@@ -38,14 +37,45 @@ fn parse_line(line: &str) -> Vec<Adjacency> {
         .collect()
 }
 
-fn create_adjacency_matrix(adjacencies: &Vec<Adjacency>) -> HashMap<(String, String), i32> {
-    let mut mat = HashMap::new();
+fn create_graph(adjacencies: &Vec<Adjacency>) -> DiGraphMap<i32, i32> {
+    let mut g = DiGraphMap::new();
+    let mut name_index: HashMap<String, i32> = HashMap::new();
+    // hacky shortcut: let's just always have shiny gold be ID 0
+    name_index.insert("shiny gold".to_string(), 0);
 
+    let mut idx = 1;
     for adj in adjacencies {
-        mat.insert((adj.parent_name.clone(), adj.child_name.clone()), adj.quantity);
+        let parent_id = match name_index.entry(adj.parent_name.clone()) {
+            Entry::Occupied(entry) => *entry.get(),
+            Entry::Vacant(entry) => {
+                entry.insert(idx);
+                idx += 1;
+                idx - 1
+            },
+        };
+
+        let child_id = match name_index.entry(adj.child_name.clone()) {
+            Entry::Occupied(entry) => *entry.get(),
+            Entry::Vacant(entry) => {
+                entry.insert(idx);
+                idx += 1;
+                idx - 1
+            }
+        };
+
+        g.add_edge(parent_id, child_id, adj.quantity);
     }
 
-    mat
+    g
+}
+
+fn bag_ways(graph: &DiGraphMap<i32, i32>, node: i32) -> i32 {
+    graph.neighbors(node)
+        .map(|neigh| {
+            let weight = graph[(node, neigh)];
+            weight + weight * bag_ways(graph, neigh)
+        })
+        .sum()
 }
 
 fn main() {
@@ -55,6 +85,8 @@ fn main() {
         .flatten()
         .collect();
 
-    let mat = create_adjacency_matrix(&adjacencies);
-    println!("{:#?}", mat);
+    let graph = create_graph(&adjacencies);
+    let result = bag_ways(&graph, 0);
+
+    println!("{}", result);
 }
